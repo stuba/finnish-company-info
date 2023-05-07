3ad9e7d0a4b48e75243fb2fe1ce2229848b1bab5
Riku <kukkaniemi.riku@gmail.com>
2023-05-07 13:21:50 +0300

    Add tests for getResponseData() method

--- tests/Integration/Service/CompanyInfoServiceTest.php
+++ tests/Integration/Service/CompanyInfoServiceTest.php
@@ -4,10 +4,15 @@ declare(strict_types=1);
 
 namespace App\Tests\Integration\Service;
 
+use App\DTO\CompanyInfo;
+use App\Exception\CompanyNotFoundException;
 use App\Exception\InvalidBusinessIdException;
+use App\Exception\UnexpectedClientDataException;
 use App\Service\CompanyInfoService;
 use Generator;
 use GuzzleHttp\Client;
+use GuzzleHttp\Exception\ClientException;
+use GuzzleHttp\Psr7\Response;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\TestDox;
 use Symfony\Bundle\FrameworkBundle\Test\KernelTestCase;
@@ -32,7 +37,41 @@ class CompanyInfoServiceTest extends KernelTestCase
     {
         self::expectException(InvalidBusinessIdException::class);
 
-        $this->service->getCompanyInformation($invalidBusinessId);
+        $this->callGetCompanyInformation($invalidBusinessId);
+    }
+
+    #[TestDox('Test that `getCompanyInformation()` method throws exception when Client throws exception')]
+    public function testGetCompanyInformationMethodThrowsExceptionWhenClientThrowsException(): void
+    {
+        $businessId = '1234567-8';
+
+        $this->client
+            ->expects(self::once())
+            ->method('get')
+            ->with('https://avoindata.prh.fi/bis/v1/' . $businessId)
+            ->willThrowException(
+                $this->getMockBuilder(ClientException::class)->disableOriginalConstructor()->getMock()
+            );
+
+        self::expectException(CompanyNotFoundException::class);
+
+        $this->callGetCompanyInformation($businessId);
+    }
+
+    #[TestDox('Test that `getCompanyInformation()` method throws exception when Client response does not have results')]
+    public function testGetCompanyInformationMethodThrowsExceptionWhenClientResponseDoesNotHaveResults(): void
+    {
+        $businessId = '1234567-8';
+
+        $this->client
+            ->expects(self::once())
+            ->method('get')
+            ->with('https://avoindata.prh.fi/bis/v1/' . $businessId)
+            ->willReturn(new Response(body: json_encode(['foo' => 'bar'])));
+
+        self::expectException(UnexpectedClientDataException::class);
+
+        $this->callGetCompanyInformation($businessId);
     }
 
     public static function dataProviderInvalidBusinessId(): Generator
@@ -41,5 +80,18 @@ class CompanyInfoServiceTest extends KernelTestCase
         yield ['11111111-1'];   // Too long
         yield ['123456a-b'];    // Letters
         yield ['         '];    // Empty spaces
+    }
+
+    private function setUpClientGetCall(string $businessId = '1234567-8'): void
+    {
+        $this->client
+            ->expects(self::once())
+            ->method('get')
+            ->with('https://avoindata.prh.fi/bis/v1/' . $businessId);
+    }
+
+    private function callGetCompanyInformation(string $businessId = '1234567-8'): CompanyInfo
+    {
+        return $this->service->getCompanyInformation($businessId);
     }
 }
