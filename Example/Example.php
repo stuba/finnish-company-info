b5de41f3024aa79daa0271fb191e20f5d8a809c7
Riku <kukkaniemi.riku@gmail.com>
2023-05-07 17:39:09 +0300

    Add example on how to use service

---
+++ Example/Example.php
@@ -0,0 +1,34 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Example;
+
+use App\Exception\CompanyInfoException;
+use App\Service\CompanyInfoService;
+
+class Example
+{
+    public function example(CompanyInfoService $companyInfoService): void
+    {
+        try {
+            $companyInfo = $companyInfoService->getCompanyInformation('1234567-8');
+        } catch (CompanyInfoException) {
+            // Handle exception
+            return;
+        }
+
+        $companyInfo->getName();                                // String
+        $companyInfo->getWebsite();                             // String or null
+
+        $currentAddress = $companyInfo->getCurrentAddress();    // Address object
+        $currentAddress->getStreet();                           // String
+        $currentAddress->getCity();                             // String
+        $currentAddress->getPostalCode();                       // String
+
+        $businessLines = $companyInfo->getBusinessLines();      // Array of BusinessLine objects
+        $businessLines[0]->getCode();                           // String
+        $businessLines[0]->getDescription();                    // String
+        $businessLines[0]->getLanguage();                       // String (language of description)
+    }
+}
